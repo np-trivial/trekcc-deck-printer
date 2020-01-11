377df11937c01c87a27d17a940b1de7a69d9eb14
np-trivial <16595428+np-trivial@users.noreply.github.com>
2020-01-11 14:09:07 -0600

    Cleanup. Added details of operations to README. Removed old code and debugging from script.

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Python script to download all the images for a deck from trekcc.org for use on D
 For example:
 
 `python.exe .\trekcc-deck-printer.py "https://www.trekcc.org/decklists/index.php?mode=view&deckID=12525"`
+
+_trekcc-deck-printer_ will create a directory based on the title of that deck, and put all the necessary images in that directory. If a deck requires multiple copies of a card, it will create that many copies. This simplifies the upload to a card printing site, so that you need not manually change the quantity of any cards.
--- trekcc-deck-printer.py
+++ trekcc-deck-printer.py
@@ -1,7 +1,5 @@
 from lxml import html
 import requests
-#import csv
-#import io
 import re
 import os
 import shutil
@@ -15,19 +13,13 @@ if __name__ == '__main__':
 	
 	try:
 		url = sys.argv[1]
-		#print(url)
 		urlmatch = re.match("https?\:\/\/(www.)?trekcc.org/.*deckID\=(\d+)",url)
 		decknamesafe = urlmatch.group(2)
-		#print(decknamesafe)
 	except:
 		print("Invalid URL")
 		exit(1)
 
-	# https://www.trekcc.org/decklists/index.php?deckID=41504&mode=download
 	page = requests.get(url)
-	#tree = html.fromstring(page.content)
-	#links = re.findall("(\dx)?\s*\<a\s+href=\"(http:\/\/trekcc.org\/\de\/index\.php\?cardID=\d+)",page.text)
-	#decknamesafe="41504"
 	
 	t = html.fromstring(page.content)
 	deckname = t.xpath("//h2/text()")[0]
@@ -52,25 +44,16 @@ if __name__ == '__main__':
 		cardpage = requests.get(cardurl)
 		cardpagetree = html.fromstring(cardpage.content)
 		imgurl = cardpagetree.xpath('//a[@href="javascript:history.go(-1)"]//img')[0].attrib['src']
-		#print(imgurl[0].attrib['src'])
 		print(imgurl)
 		filename = re.search("\/([^/]+)$",imgurl).group(1)
-		print(filename)
 		imgcontent = requests.get(imgurl)
 		image_file = tempfile.TemporaryFile()
 		image_file.write(imgcontent.content)
 		my_image = Image.open(image_file)
+		# omitted because this conversion worked poorly. 
+		#TODO: revisit CMYK conversion
 		#my_image = my_image.convert(mode="CMYK")
 		my_image = my_image.resize((825,1125),resample=Image.LANCZOS)
 		for i in range(qty):
-			#dest_file = open(decknamesafe + "/" + str(i) + "_" + filename, "wb")
-			#dest_file.write(imgcontent.content)
-			#shutil.copyfileobj(imgcontent.content, dest_file)
-			#dest_file.close()
 			my_image.save(decknamesafe + "/" + str(i) + "_" + filename)
 
-		#image_url = re.findall("https?:\/\/www.trekcc.org\/\de\/cardimages\/[^/]+",cardpage.text)
-		#print("cardurl: " , image_url)
-		# download image
-		# https://www.trekcc.org/2e/cardimages/STVE-EN43004.jpg
-		#break
